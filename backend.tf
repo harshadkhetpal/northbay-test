33be2bbb1e81b3f8452715f5ff245136baf5b186
harshadkhetpal <harshad.khetpal@icloud.com>
2025-10-28 00:27:55 +0530

    fixed the changes

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatebackend"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_azuread_auth      = true
-  }
-}
